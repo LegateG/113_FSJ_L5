45f1ddf5723b7d47124c1d791b1867f0f9e1f194
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-06-19 15:41:11 -0400

    Create app.js

---
+++ lab5-modules-practical/app.js
@@ -0,0 +1,6 @@
+import { add, subtract } from './mathModule.js';
+import { toUpperCase, toLowerCase } from './stringModule.js';
+ 
+// Calling the functions and logging results
+console.log('Add: ', add(5, 3));  // Testing add function
+console.log('Uppercase: ', toUpperCase('hello'));  // Testing string manipulation
