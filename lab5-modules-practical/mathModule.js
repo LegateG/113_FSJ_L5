ba27d2bc780fd433fd66a1031d74ae40db29dc93
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-06-20 00:00:01 -0400

    Update mathModule.js

--- lab5-modules-practical/mathModule.js
+++ lab5-modules-practical/mathModule.js
@@ -1,3 +1,8 @@
+import {toUpperCase} from './stringModule.js';
+export function addAndLogUpper(a,b) {
+    const result = add(a,b);
+    console.log(toUpperCase(result.toString()));
+}
 export function add(x,y) {
     return x + y;
 }
