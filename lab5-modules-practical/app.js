24eee2c2216555818871ba0d5ecae9acecce8a89
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-06-20 08:36:57 -0400

    Update app.js

--- lab5-modules-practical/app.js
+++ lab5-modules-practical/app.js
@@ -1,12 +1,12 @@
-import multiply,{ add, subtract, addAndLogUpper  } from './mathModule.js';
+import multiply, { add, subtract, addAndLogUpper } from './mathModule.js';
 import { toUpperCase, toLowerCase } from './stringModule.js';
 import { findMax, reverseArray } from './arrayModule.js';
- 
+
 // Calling the functions and logging results
-console.log('Multiply:',multiply(5,6)); // Testing the default export
+console.log('Multiply:', multiply(5, 6)); // Testing the default export
 console.log('Add: ', add(5, 3));  // Testing add function
 console.log('Uppercase: ', toUpperCase('hello'));  // Testing string manipulation
-console.log('Max: ', findMax([1, 2, 3, 4, 5]));  // Testind findMax
+console.log('Max: ', findMax([1, 2, 3, 4, 5]));  // Testing findMax
 console.log('Reversed: ', reverseArray([1, 2, 3]));  // Testing reverseArray
- 
+
 addAndLogUpper(10, 20);  // Testing the new function
