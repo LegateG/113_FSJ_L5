3c749de84e6d6d3bd69a25330bd1b3b5810dae1e
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-06-20 00:00:42 -0400

    Update app.js

--- lab5-modules-practical/app.js
+++ lab5-modules-practical/app.js
@@ -7,3 +7,7 @@ console.log('Add: ', add(5, 3));  // Testing add function
 console.log('Uppercase: ', toUpperCase('hello'));  // Testing string manipulation
 console.log('Max: ', findMax([1, 2, 3, 4, 5]));  // Testind findMax
 console.log('Reversed: ', reverseArray([1, 2, 3]));  // Testing reverseArray
+
+import { addAndLogUpper } from './mathModule.js';
+ 
+addAndLogUpper(10, 20);  // Testing the new function
