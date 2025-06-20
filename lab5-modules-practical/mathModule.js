9034d36ab60d306b70a5c32460aa226fca2ef001
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-06-20 11:16:39 -0400

    Update mathModule.js

--- lab5-modules-practical/mathModule.js
+++ lab5-modules-practical/mathModule.js
@@ -1,14 +1,14 @@
 import {toUpperCase} from './stringModule.js';
-export function addAndLogUpper(a,b) {
-    const result = add(a,b);
-    console.log(toUpperCase(result.toString()));
-}
 export function add(x,y) {
     return x + y;
 }
 export function subtract(x,y) {
     return x - y;
 }
-export default function multiply(a, b) {
-    return a * b;
+export default function multiply(x, y) {
+    return x * y;
+}
+export function addAndLogUpper(x,y) {
+    const result = add(x,y);
+    console.log(`Uppercase result of addAndLogUpper: ${toUpperCase(result.toString())}`);
 }
