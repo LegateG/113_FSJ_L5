4b4a375d4823b839399f6dd741e9ab578b1cd6e0
Gorkem Bayar <97575399+LegateG@users.noreply.github.com>
2025-06-19 23:49:41 -0400

    Update app.js

--- lab5-modules-practical/app.js
+++ lab5-modules-practical/app.js
@@ -1,6 +1,7 @@
-import { add, subtract } from './mathModule.js';
+import multiply,{ add, subtract } from './mathModule.js';
 import { toUpperCase, toLowerCase } from './stringModule.js';
  
 // Calling the functions and logging results
+console.log('Multiply:',multiply(5,6)); // Testing the default export
 console.log('Add: ', add(5, 3));  // Testing add function
 console.log('Uppercase: ', toUpperCase('hello'));  // Testing string manipulation
